5f320841ef464a819a1a47baba2820e291503e73
Moralde-Sama <moralde.sama@gmail.com>
2019-08-18 21:14:09 +0800

    remove linkedIn methods

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -12,37 +12,6 @@ export class AboutMeComponent implements OnInit {
   ngOnInit() {
   }
 
-  ngAfterViewInit(): void {
-    let count = 0;
-    const interval = setInterval(() => {
-      try {
-        const anchor_tag = document.getElementsByClassName('LI-footer LI-row')[0].firstChild;
-        this.renderer.setAttribute(anchor_tag, 'target', '_blank');
-        this.renderer.setAttribute(anchor_tag, 'rel', 'noopener noreferrer')
-        clearInterval(interval);
-      } catch(error) {
-        if(count > 5)
-          clearInterval(interval);
-      }
-      count++;
-    }, 1000);
-    
-  }
-
-  showLinkedIn(): void {
-    const linkedin_elem = document.getElementsByClassName('LI-badge-container-horizontal-dark');
-    if(linkedin_elem[0].className.includes('show')){
-      this.renderer.removeClass(linkedin_elem[0], 'show')
-      this.renderer.addClass(linkedin_elem[0], 'hide')
-    }
-    else if(linkedin_elem[0].className.includes('hide')){
-      this.renderer.removeClass(linkedin_elem[0], 'hide')
-      this.renderer.addClass(linkedin_elem[0], 'show')
-    } else {
-      this.renderer.addClass(linkedin_elem[0], 'show')
-    }
-  }
-
   showEmail(): void {
     const gmail_elem = document.getElementsByClassName('gmail')[0];
     if(gmail_elem.className.includes('show'))
