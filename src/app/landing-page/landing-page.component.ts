674c315ce22c41cca23e207ade5a19a9aaf1ccfb
Moralde-Sama <moralde.sama@gmail.com>
2019-08-30 00:17:52 +0800

    update fixed for white spacing on ie and edge

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: []
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  constructor(private _renderer: Renderer2) { }
 
   ngOnInit() {
+    // for ie and edge
+    // tslint:disable-next-line: variable-name
+    const page_container_elem = document.querySelectorAll('div.page-container')[0];
+    this._renderer.addClass(page_container_elem, 'fixed');
   }
 
+  ngOnDestroy(): void {
+    // for ie and edge
+    // tslint:disable-next-line: variable-name
+    const page_container_elem = document.querySelectorAll('div.page-container.fixed')[0];
+    this._renderer.removeClass(page_container_elem, 'fixed');
+  }
 }
