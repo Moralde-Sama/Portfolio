b411a3431a3afb22397a7267fc5feb3823dbb6f1
Moralde-Sama <moralde.sama@gmail.com>
2020-01-30 10:47:49 +0800

    removed angular files for nextjs

--- src/app/directive/animation-delay.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimationDelayDirective } from './animation-delay.directive';
-
-describe('AnimationDelayDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AnimationDelayDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/work-experience/work-experience.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface WorkExperience {
-    start: string;
-    end: string;
-    company: string;
-    position: string;
-    experience: string;
-    description: string;
-}
